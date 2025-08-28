af07007522e96131c9b439005737b3606305b53d
Samuelle St-Onge <samuellestonge@gmail.com>
2025-08-28 17:33:50 -0400

    ajout des fichiers .py et images

---
+++ cars.py
@@ -0,0 +1,23 @@
+import pygame
+from config import CAR_COLORS, CARS_SIZE
+
+# Dictionnaire pour les voitures de différentes couleurs et directions
+cars_dict = {
+    "left": [],
+    "right": []
+}
+
+# ======================== PARTIE 2.1 ========================
+# TODO : 
+# 1. Parcourir la liste des couleurs (CAR_COLORS) à l'aide d'une boucle
+# 2. Pour chaque couleur :
+#    - Chargez l'image vers la droite ("_right") et celle vers la gauche ("_left") à l'aide de pygame.image.load().
+#    - Redimensionnez chaque image à CARS_SIZE à l’aide de la fonction pygame.transform.scale().
+#    - Ajoutez les images redimensionnées dans la bonne liste ("left" ou "right") du dictionnaire cars_dict.
+
+# Écrire votre code ici : 
+
+
+
+
+# ===============================================================
---
+++ wood_logs.py
@@ -0,0 +1,20 @@
+import pygame
+from config import LOG_SIZES
+
+# ======================== PARTIE 3.1 ===========================
+# 
+# TODO : Définir un dictionnaire pour les bûches en bois avec trois tailles différentes : petite, moyenne et longue
+# 
+# Étapes à suivre : 
+# - Créer un dictionnaire nommé `logs_dict` avec les trois clés suivantes : 
+#      - short
+#      - medium
+#      - long 
+#
+# - Utilisez "pygame.image.load()" pour charger l'image "log.png" (à partir du dossier "images/")
+#   trois fois, c'est-à-dire une fois pour chaque taille.
+# - Redimensionnez (avec "pygame.transform.scale") chaque image en utilisant les clés du dictionnaire LOG_SIZES. 
+
+logs_dict = { }
+
+# ===============================================================
